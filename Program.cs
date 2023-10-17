06dac226185abac87520c8474bba75e464867e66
baggebom <144337233+baggebom@users.noreply.github.com>
2023-10-17 15:05:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Upggift
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge hade du tänkt att hyra bilen? Du får inte hyra bilen längre än 11 timmar");
+            int hyra= int.Parse(Console.ReadLine());
+
+            if (hyra !< 12)
+            {
+                Console.WriteLine("Kostnaden för hyrningen är " + (hyra * 80) + " kr.");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte hyra den så länge!");
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
